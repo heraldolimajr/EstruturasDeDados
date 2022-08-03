11bf81ecdd9b5ab653b2a8252e737694f6481496
Heraldo Jr <heraldo.info@gmail.com>
2022-08-03 18:31:56 -0300

    Create Pilha.java

---
+++ CÓDIGOS/PILHAS ESTÁTICAS/Pilha.java
@@ -0,0 +1,46 @@
+
+public class Pilha {
+	private Object elementos[];
+	private int topo;
+	
+	public Pilha(int capacidade) {
+		this.topo=-1;
+		this.elementos = new Object[capacidade];
+	}
+	
+	//INSERE
+	public boolean push(Object elemento) {
+		if(this.topo < this.elementos.length-1) {
+			this.topo++;
+			this.elementos[topo] = elemento;
+			return true;
+		}
+		return false;
+	}
+	
+	//VERIFICA SE ESTÁ VAZIA
+	public boolean isEmpty() {
+		return topo<0;
+	}
+	
+	//TAMANHO
+	public int size() {
+		if(this.isEmpty()) return 0;
+		return this.topo+1;
+	}
+	
+	//REMOVE TOPO
+	public Object pop() {
+		if (this.isEmpty()) return null;
+		return this.elementos[this.topo--];
+	}
+	
+	public Object top() {
+		if (this.isEmpty()) return null;
+		return this.elementos[this.topo];
+	}
+	
+}
+
+
+
